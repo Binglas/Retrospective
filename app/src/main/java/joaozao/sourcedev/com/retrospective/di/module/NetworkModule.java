8bf725ace5c8f1ae48b84d07028f8d278adad0c3
jzao <joao.zao@blip.pt>
2017-12-16 18:35:08 +0000

    Add gitlab retrofit dagger implementation

--- app/src/main/java/joaozao/sourcedev/com/retrospective/di/module/NetworkModule.java
+++ app/src/main/java/joaozao/sourcedev/com/retrospective/di/module/NetworkModule.java
@@ -9,14 +9,18 @@ import javax.inject.Singleton;
 import dagger.Module;
 import dagger.Provides;
 import joaozao.sourcedev.com.retrospective.di.qualifier.DefaultOkHttpClient;
+import joaozao.sourcedev.com.retrospective.di.qualifier.GitlabRetrofit;
 import okhttp3.Cache;
 import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
 
 @Module
 public class NetworkModule {
 
     private static final int DEFAULT_CACHE_SIZE = 10 * 1024 * 1024; // 10 Mib
     private static final int DEFAULT_TIMEOUT = 30; // Seconds
+
+    private static final String BASE_URL_GITLAB = "https://api.gitlab.com/";
 
     private NetworkModule() {
         // Disallow instantiation with empty private constructor
@@ -37,6 +41,17 @@ public class NetworkModule {
                 .connectTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
                 .readTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
                 .writeTimeout(DEFAULT_TIMEOUT, TimeUnit.SECONDS)
+                .build();
+    }
+
+    @Provides
+    @Singleton
+    @GitlabRetrofit
+    static Retrofit provideGitlabRetrofit(@DefaultOkHttpClient OkHttpClient okHttpClient) {
+        return new Retrofit.Builder()
+                .client(okHttpClient)
+                .baseUrl(BASE_URL_GITLAB)
+                .addConverterFactory(MoshiConverterFactory.create())
                 .build();
     }
 
---
+++ app/src/main/java/joaozao/sourcedev/com/retrospective/di/qualifier/GitlabRetrofit.java
@@ -0,0 +1,15 @@
+package joaozao.sourcedev.com.retrospective.di.qualifier;
+
+
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
+import javax.inject.Qualifier;
+
+@Qualifier
+@Documented
+@Retention(RetentionPolicy.RUNTIME)
+public @interface GitlabRetrofit {
+
+}
